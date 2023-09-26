2ddc0491b7cb4b0a6f8f66cde5e5fb02e5efd5bc
hanggarahadianto <93454559+hanggarahadianto@users.noreply.github.com>
2023-09-27 04:07:32 +0700

    adding navbar

--- types/building.d.ts
+++ types/building.d.ts
@@ -1,6 +1,6 @@
 export interface Building {
-  id: number;
-  BuildingName: string;
-  BuildingDescription: string;
-  ImageURL: string;
+  id: string;
+  name: string;
+  content: string;
+  image: string;
 }
